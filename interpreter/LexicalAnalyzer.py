02e952c48ec30932e1beb6cf084d60819e29dfc4
Henry Jiang <h243jian@uwaterloo.ca>
2020-12-28 14:14:03 -0800

    Added LexicalAnalyzer.py Testing Suite

--- interpreter/LexicalAnalyzer.py
+++ interpreter/LexicalAnalyzer.py
@@ -3,8 +3,11 @@
 The lexical analyzer module for the interpreted language. This module implements
 the parsing for Abstract Syntax Tree
 
-This module can be imported as a module and contains the following functions:
+This module can be imported as a module and contains the following function:
     * parse       : returns the parsed Abstract Syntax Tree
+
+    Functions
+    ---------
     * tokenize    : returns the program parsed as a list of tokens
     * syntax_tree : generates the Abstract Syntax Tree from list of tokens
     * atomize     : parses tokens to the appropriate datatype
@@ -15,14 +18,65 @@ from typing import List, Any
 
 
 def tokenize(program: str) -> List[str]:
-    pass
+    """ Tokenizes the program text into a list of Tokens
+
+    Preformat program text and then parses the program into the a list of
+    tokens based on spaces and brackets. Statements are postfixed by ';'
+
+    :param program: the program text to be tokenized
+    :type  program: str
+
+    :returns: a list of strings of tokens
+    :rtype: list
+    """
+
+    # Performatting the program text to remove tabs and returns
+    program = re.sub(r'[\n\t]', '', program.replace(';', ''))
+
+    # define language seperator
+    seperator = ['(', ')', '[', ']', '{', '}']
+    for sep in seperator:
+        program = program.replace(sep, f' {sep} ')
+
+    # remove duplicate spaces
+    program = re.sub(r'\s+', ' ', program)
+
+    # tokenize program
+    return program.split()
+    
 
 
 def abstract_syntax_tree(tokens: List[str]) -> List[Any]:
     pass
-
+    
 
 def parse(program: str) -> List[Any]:
+    """ Parses program text into an Abstract Syntax Tree
+    
+    :param program: the program text
+    :type  program: str
+
+    :returns: the Abstract Syntax Tree parsed from the program text
+    :retype: List[Any] Abstract Syntax Tree
+    """
+    
     return abstract_syntax_tree(tokenize(program))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ test/test_lexicalanalyzer.py
@@ -0,0 +1,50 @@
+import unittest
+
+from interpreter.LexicalAnalyzer import tokenize
+
+class TestLexicalAnalyzer(unittest.TestCase):
+
+    # tokenize unittest
+    def test_tokenize_literal(self):
+        # empty string returns empty tokens
+        self.assertEqual(tokenize(''), [])
+        self.assertEqual(tokenize('123'), ['123'])
+        self.assertEqual(tokenize('sqrt(123)'), ['sqrt', '(', '123', ')'])
+        # binding expression
+        self.assertEqual(tokenize('bind num to 1234'), ['bind', 'num', 'to', '1234'])
+
+    def test_tokenize_list(self):
+        self.assertEqual(tokenize('list(1 2 3 4 5 6 7 8 9)'),
+                         ['list', '(', '1', '2', '3', '4', '5', '6', '7', '8', '9', ')'])
+        self.assertEqual(tokenize('bind array to list(1 2 3 4 5 6 7 8 9)'),
+                         ['bind', 'array', 'to', 'list', '(', '1', '2', '3', '4',
+                          '5', '6', '7', '8', '9', ')'])
+
+    def test_tokenize_lambda(self):
+        # lambda expressions
+        self.assertEqual(tokenize('lambda({x} -> x)'),
+                         ['lambda', '(', '{', 'x', '}', '->', 'x', ')'])
+        self.assertEqual(tokenize('(lambda({x} -> x))(5)'),
+                         ['(','lambda', '(', '{', 'x', '}', '->', 'x', ')',
+                          ')', '(', '5', ')'])
+        self.assertEqual(tokenize('[(lambda({x} -> lambda({y} -> +(x y))))(1)](4)'),
+                         ['[', '(', 'lambda', '(', '{', 'x', '}', '->', 'lambda',
+                          '(', '{', 'y', '}', '->', '+', '(', 'x', 'y', ')', ')', ')',
+                          ')', '(', '1', ')', ']', '(', '4', ')'])
+        
+    def test_tokenize_functions(self):
+        # function expressions
+        self.assertEqual(tokenize('bind add to lambda({x y} -> +(x y))'),
+                         ['bind', 'add', 'to', 'lambda', '(', '{', 'x', 'y', '}', '->',
+                          '+', '(', 'x', 'y', ')', ')'])
+        self.assertEqual(tokenize("""bind ! to lambda({x} -> 
+                                        if(<=(x 1) 
+                                            1 
+                                            *(x !(-(x 1)))));"""),
+                         ['bind', '!', 'to', 'lambda', '(', '{', 'x', '}', '->',
+                          'if', '(', '<=', '(', 'x', '1', ')', '1', '*', '(',
+                          'x', '!', '(', '-', '(', 'x', '1', ')', ')', ')', ')', ')'])
+
+    #TODO: Implement the String testing Suite
+    #TODO: Implement the Struct testing Suite
+        
